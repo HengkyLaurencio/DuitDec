8b379eff105fb39a000c56d14269abc5cbcd6c9d
SamuelSupardjo1 <SamuelSupardjo@gmail.com>
2024-11-24 22:12:06 +0700

    added add outcome feature

--- backend/src/features/transactions/transactionsController.js
+++ backend/src/features/transactions/transactionsController.js
@@ -15,6 +15,20 @@ class TransactionsController {
             res.status(500).json({ message: error.message });
         }
     }
+
+    static async addOutcome(req, res) {
+        try {
+            // user id harus diambil dari tabel user gabole manual
+            // category id harus diambil dari tabel category juga
+            const result = await TransactionsService.addTransaction({ 
+                ...req.body, 
+                transaction_type: "outcome" 
+            });
+            res.status(201).json({ message: "Outcome added successfully", data: result });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
 }
   
 module.exports = TransactionsController;
--- backend/src/features/transactions/transactionsRoute.js
+++ backend/src/features/transactions/transactionsRoute.js
@@ -6,7 +6,7 @@ const TransactionsController = require("./transactionsController");
 app.use(express.json());
 
 router.post("/income", TransactionsController.addIncome)
-// router.post("/outcome", TransactionsController.addOutcome)
+router.post("/outcome", TransactionsController.addOutcome)
 // router.get("/",TransactionsController.getTransactions)
 
 module.exports = router;
